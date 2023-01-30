194782469850afa528d43ad67b29ae5ebdd46f2b
gryzlegrizz <gryzlegrizz@gmail.com>
2023-01-30 19:33:00 +0700

    update

---
+++ UI/classview/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/manual-css/style.css">
+    <link rel="icon" type="image/x-icon" href="assets/img/SA-Logo-RemovedBG.png">
+    <title>Smart.id</title>
+  </head>
+  <body>
